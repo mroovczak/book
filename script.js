aeeb7f3a043a628a2946a85b85d3eeab4f4b5b31
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-26 21:15:02 +0200

    add read and delete functionality

--- script.js
+++ script.js
@@ -1,41 +1,72 @@
-
 let myLibrary = [];
 
-function Book(title, author, pages, read = false) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+function Book(title, author, pages, id, read = false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
 }
 
 function addBookToLibrary() {
-  
+
 }
-for (let i=0; i<15; i++){
-    myLibrary.push(new Book('KSIONDXZ','DUPA JASU','232',false))
+for (let i = 0; i < 15; i++) {
+    myLibrary.push(new Book('KSIONDXZ', `DUPA JASU ${i}`, '232', i, true))
 }
+
 function drawLibrary() {
-    index=0;
     const library = document.querySelector(".library")
     myLibrary.forEach(book => {
         // console.log(book);
         const newCard = document.createElement("div");
-        newCard.setAttribute("id",index);
-        newCard.setAttribute("class","card");
-        newCard.setAttribute("style","width: 450px;");
-        index++;
-        newCard.innerHTML = 
-        `<div class="card-divider">
+        newCard.setAttribute("data-id", book.id);
+        newCard.setAttribute("class", "card");
+        newCard.setAttribute("style", "width: 450px;");
+        newCard.innerHTML =
+            `<div class="card-divider">
             ${book.title}
         </div>
-        <img src="assets/img/generic/rectangle-1.jpg">
         <div class="card-section">
           <h4>${book.author}</h4>
           <p>${book.pages}</p>
-          <p>${book.read? "read": "not read"}</p>
+          <button class=" book"
+          data-id=${book.id}
+          data-read=${book.read}
+          >${book.read? "read": "not read"}</button>
+          <button class="delete-book"
+          data-id=${book.id}
+          data-read=${book.read}
+          >delete book</button>
         </div>`;
-        console.log(library);
+        //console.log(library);
         library.appendChild(newCard);
     });
 }
+
 document.body.onload = drawLibrary();
+let readButtons = document.querySelectorAll('.book');
+readButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const book = myLibrary.find(b => {
+            return (b.id == e.target.dataset.id);
+        })
+        book.read = !book.read;
+        e.target.dataset.read = e.target.dataset.read == 'true'? 'false' : 'true';
+        e.target.innerHTML = book.read? "read" : "not read"
+        // console.log(e.target.dataset.read);
+        // console.log(book);
+    });
+});
+
+let deleteButtons = document.querySelectorAll('.delete-book');
+deleteButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const book = myLibrary.find(b => {
+            return (b.id == e.target.dataset.id);
+        })
+        const card = document.querySelector(`[data-id="${e.target.dataset.id}"]`)
+        // book;
+        card.remove();
+    });
+});
