a00566afe9c5bd0d4fe08ff09d84eb427901359f
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-28 23:18:09 +0200

    add new books functionality

--- script.js
+++ script.js
@@ -9,59 +9,83 @@ function Book(title, author, pages, id, read = false) {
 }
 
 function addBookToLibrary(values) {
-    myLibrary.push(new Book(...values));
-    drawLibrary();
+    const newBook = new Book(...values,myLibrary.length);
+    myLibrary.push(newBook);
+    drawBook(newBook);
     console.table(myLibrary);
 }
 for (let i = 0; i < 15; i++) {
     myLibrary.push(new Book('KSIONDXZ', `DUPA JASU ${i}`, '232', i, true))
 }
 
-function drawLibrary() {
+function drawBook(book) {
     const library = document.querySelector(".library")
-    library.innerHTML = '';
+    const newCard = document.createElement("div");
+    newCard.setAttribute("data-id", book.id);
+    newCard.setAttribute("class", "card");
+    newCard.setAttribute("style", "width: 450px;");
+    newCard.innerHTML =
+        `<div class="card-divider">
+        ${book.title}
+    </div>
+    <div class="card-section">
+      <h4>${book.author}</h4>
+      <p>Pages: ${book.pages}</p>
+      <button class=" book"
+      data-id=${book.id}
+      data-read=${book.read}
+      >${book.read? "read": "not read"}</button>
+      <button class="delete-book"
+      data-id=${book.id}
+      data-read="false"
+      >delete book</button>
+    </div>`;
+    //console.log(library);
+    library.appendChild(newCard);
+    addDeleteButtonEvent(newCard);
+    addReadButtonEvent(newCard);
+}
+
+function drawLibrary() {
+    
+    // library.innerHTML = '';
     myLibrary.forEach(book => {
         // console.log(book);
-        const newCard = document.createElement("div");
-        newCard.setAttribute("data-id", book.id);
-        newCard.setAttribute("class", "card");
-        newCard.setAttribute("style", "width: 450px;");
-        newCard.innerHTML =
-            `<div class="card-divider">
-            ${book.title}
-        </div>
-        <div class="card-section">
-          <h4>${book.author}</h4>
-          <p>Pages: ${book.pages}</p>
-          <button class=" book"
-          data-id=${book.id}
-          data-read=${book.read}
-          >${book.read? "read": "not read"}</button>
-          <button class="delete-book"
-          data-id=${book.id}
-          data-read=${book.read}
-          >delete book</button>
-        </div>`;
-        //console.log(library);
-        library.appendChild(newCard);
+        drawBook(book);
+        // addReadButtonEvent(book);
     });
 }
 
 document.body.onload = drawLibrary();
-let readButtons = document.querySelectorAll('.book');
-readButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
+// let readButtons = document.querySelectorAll('.book');
+// readButtons.forEach(button => {
+//     addReadButtonEvent(button)
+// });
+function addReadButtonEvent(element){
+    let readButton = element.querySelector('.book');
+    readButton.addEventListener("click", (e) => {
         const book = myLibrary.find(b => {
             return (b.id == e.target.dataset.id);
         })
         book.read = !book.read;
         e.target.dataset.read = e.target.dataset.read == 'true'? 'false' : 'true';
         e.target.innerHTML = book.read? "read" : "not read"
-        // console.log(e.target.dataset.read);
+        console.log(e.target.dataset.read);
         // console.log(book);
     });
-});
-
+}
+function addDeleteButtonEvent(element){
+    let deleteButton = element.querySelector('.delete-book');
+    deleteButton.addEventListener("click", (e) => {
+        const book = myLibrary.find(b => {
+            return (b.id == e.target.dataset.id);
+        })
+        const card = document.querySelector(`[data-id="${e.target.dataset.id}"]`)
+        myLibrary = myLibrary.filter(book => book.id != e.target.dataset.id);
+        card.remove();
+        console.table(myLibrary);
+    });
+}
 let deleteButtons = document.querySelectorAll('.delete-book');
 deleteButtons.forEach(button => {
     button.addEventListener("click", (e) => {
